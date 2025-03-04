ff3d1560fd4f325c8ea3f6e9ad155ca009643857
杨子豪 <beiding110@gmail.com>
2025-03-04 18:01:25 +0800

    【修改】调整简单二值化算法；

--- utils/simpleBinarization.js
+++ utils/simpleBinarization.js
@@ -5,7 +5,15 @@ module.exports = function simpleBinarization(image) {
     const { data } = image.bitmap;
 
     for (let i = 0; i < data.length; i += 4) {
-        const value = data[i] <= threshold ? 0 : 255;
+        const value = average(data[i], data[i + 1], data[i + 2]) <= threshold ? 0 : 255;
         data[i] = data[i + 1] = data[i + 2] = value;
     }
 };
+
+function average(...args) {
+    return (
+        args.reduce((sum, cur) => {
+            return sum + cur;
+        }, 0) / args.length
+    );
+}
