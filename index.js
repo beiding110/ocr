e82b26b5109a8ccff4142de76fad6448488e5a59
杨子豪 <beiding110@gmail.com>
2025-03-10 14:47:27 +0800

    【修改】调整程序写法，拆分测试内容；

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const { Jimp } = require('jimp');
 const Tesseract = require('tesseract.js');
-const path = require('path');
 
 const simpleBinarization = require('./utils/simpleBinarization.js');
 const clearBoundaryColor = require('./utils/clearBoundaryColor.js');
@@ -11,8 +10,11 @@ const fillHole = require('./utils/fillHole.js');
 const toolColorAnalysis = require('./utils/toolColorAnalysis.js');
 
 class CaptchaRecognizer {
-    constructor() {
+    constructor(obj) {
         this.worker = null;
+
+        this.langPath = obj.path;
+        this.workerPath  = obj.workerPath;
     }
 
     async terminate() {
@@ -74,8 +76,18 @@ class CaptchaRecognizer {
     }
 
     async recognize(imagePath) {
+        let settings = {};
+
+        if (this.langPath) {
+            settings.langPath = this.langPath;
+        }
+
+        if (this.workerPath) {
+            settings.workerPath = this.workerPath;
+        }
+
         this.worker = await Tesseract.createWorker('eng', 1, {
-            langPath: path.resolve(__dirname, './tessdata-gh-pages/4.0.0_best'),
+            ...settings,
             // logger: (m) => console.log(m), // 可选日志记录
         });
 
@@ -105,49 +117,4 @@ class CaptchaRecognizer {
     }
 }
 
-// 使用示例
-(async () => {
-    var ts = new Date().getTime();
-
-    function compareResult(result, answer) {
-        if (result.toLowerCase() === answer.toLowerCase()) {
-            return `${result} √`;
-        }
-
-        return `${result} × ${answer}`;
-    }
-
-    const recognizer = new CaptchaRecognizer();
-
-    // const result0 = await recognizer.recognize('test0.png');
-    // console.log('识别结果0:', result0);
-
-    const result1 = await recognizer.recognize('./images/test1.png');
-    console.log('识别结果1:', compareResult(result1, 'NJBB'));
-
-    const result2 = await recognizer.recognize('./images/test2.png');
-    console.log('识别结果2:', compareResult(result2, 'AZSd'));
-
-    const result3 = await recognizer.recognize('./images/test3.png');
-    console.log('识别结果3:', compareResult(result3, '8MvHb'));
-
-    const result4 = await recognizer.recognize('./images/test4.png');
-    console.log('识别结果4:', compareResult(result4, 'vimowiy'));
-
-    const result5 = await recognizer.recognize('./images/test5.png');
-    console.log('识别结果5:', compareResult(result5, 'iucz5'));
-
-    const result6 = await recognizer.recognize('./images/test6.png');
-    console.log('识别结果6:', compareResult(result6, 'm3dv8'));
-
-    // const result7 = await recognizer.recognize('./images/test7.png');
-    // console.log('识别结果7:', result7);
-
-    const result8 = await recognizer.recognize('./images/test8.png');
-    console.log('识别结果8:', compareResult(result8, 'WXB4'));
-
-    const result9 = await recognizer.recognize('./images/test9.png');
-    console.log('识别结果9:', compareResult(result9, 'MGRg'));
-
-    console.log(`耗时：${(new Date().getTime() - ts) / 1000}s`);
-})();
+module.exports = CaptchaRecognizer;
